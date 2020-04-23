fb712673ca129331b3d6744a38cb69084db272cf
Ashir <ashirq@outlook.com>
2020-04-23 15:14:08 +0100

    App now diplaying generation mix in a chart.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div id="app">
+  <div class="" id="app">
     <h1>Energy Mix App</h1>
-    <!-- <p>{{ energymix }}</p> -->
-    <detail-view v-if="energymix" :energymix="energymix"></detail-view>
+    <div class="container">
+
+
+    <section class="detail">
+      <detail-view v-if="energymix" :energymix="energymix"></detail-view>
+
+    </section>
+    <section class="graph">
+      <graph-view v-if="energymix" :graphData="energymix"></graph-view>
+    </section>
+      </div>
   </div>
 </template>
 
 <script>
 
 import Details from './components/Details.vue';
+import Graph from './components/Graph.vue';
 
 export default {
   name: 'App',
   data(){
     return{
-      energymix: []
+      energymix: null,
     }
   },
   mounted(){
@@ -23,12 +33,29 @@ export default {
     .then(result => this.energymix = result.data)
   },
   components: {
-    "detail-view": Details
+    "detail-view": Details,
+    "graph-view": Graph
   }
 }
 </script>
 
 <style>
+h1 {
+  text-align: center;
+}
+.container {
+  display:flex;
+
+  justify-content: space-around;
+  /* flex-direction: column; */
+}
+ .detail{
+   /* width:50%; */
+ }
+ .graph{
+   display: flex;
+   /* width:100%; */
+ }
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,9 +1,47 @@
 <template lang="html">
+  <GChart
+    type="ColumnChart"
+    :data="chartData"
+    :options="chartOptions"
 
+  />
 </template>
 
 <script>
+import { GChart } from 'vue-google-charts';
+
 export default {
+  name:"graph-view",
+  props:['graphData'],
+  data() {
+    return {
+      inputData: this.graphData,
+      chartData: this.populateChart(),
+      chartOptions: {
+        chart: {
+          title: 'Energy Mix',
+          subtitle: '%'
+        }
+      }
+    };
+  },
+  methods: {
+    populateChart: function(){
+      const input = [["Fuel", "%"]];
+      this.graphData.generationmix.forEach((item, i) => {
+        input.push([item.fuel, item.perc]);
+      });
+
+      console.log(input);
+      return input;
+    }
+
+
+  },
+
+  components: {
+    GChart
+  }
 }
 </script>
 
